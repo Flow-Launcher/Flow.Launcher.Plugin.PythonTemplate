5b0fab7cd0287ceeeed0f120add2b7c1f64bab9f
Zero <zeroto521@gmail.com>
2020-05-21 11:47:26 +0800

    :bug: fix load .env

--- Wox.Plugin.PythonTemplate/plugin/constants.py
+++ Wox.Plugin.PythonTemplate/plugin/constants.py
@@ -3,7 +3,16 @@
 
 import os
 
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
 
 ICON_PATH = 'assets/favicon.ico'
 
-CONFIG = os.getenv('CONFIG', 'default config')  # The default value can work, if no user config.
+# The default value can work, if no user config.
+CONFIG = os.getenv('CONFIG', 'default config')
