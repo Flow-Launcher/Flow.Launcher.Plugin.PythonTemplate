dd13448e12f0ceba7b4bd928a92fe351e7c0ec21
Zero <Zeroto521@gmail.com>
2021-08-13 17:44:21 +0800

    use oop path replace fixed string

--- Flow.Launcher.Plugin.PythonTemplate/commands.py
+++ Flow.Launcher.Plugin.PythonTemplate/commands.py
@@ -35,6 +35,8 @@ lib_path.mkdir(exist_ok=True)
 build_ignore_path = basedir / ".buildignore"
 build_ignore_path.touch()  # if no existed, would be created
 entry_src = basedir / PLUGIN_EXECUTE_FILENAME
+translations_path = basedir / "plugin/translations"
+plugin_info_path = basedir / "plugin.json"
 
 plugin_infos = {
     "ID": PLUGIN_ID,
@@ -98,7 +100,7 @@ def init(locale):
 
     if os.system("pybabel extract -F babel.cfg -k _l -o messages.pot ."):
         raise RuntimeError("extract command failed")
-    if os.system(f"pybabel init -i messages.pot -d plugin/translations -l {locale}"):
+    if os.system(f"pybabel init -i messages.pot -d {translations_path} -l {locale}"):
         raise RuntimeError("init command failed")
     os.remove("messages.pot")
 
@@ -110,7 +112,7 @@ def update():
     """Update all languages."""
     if os.system("pybabel extract -F babel.cfg -k _l -o messages.pot ."):
         raise RuntimeError("extract command failed")
-    if os.system("pybabel update -i messages.pot -d plugin/translations"):
+    if os.system(f"pybabel update -i messages.pot -d {translations_path}"):
         raise RuntimeError("update command failed")
     os.remove("messages.pot")
 
@@ -121,7 +123,7 @@ def update():
 def compile():
     """Compile all languages."""
 
-    if os.system("pybabel compile -d plugin/translations"):
+    if os.system(f"pybabel compile -d {translations_path}"):
         raise RuntimeError("compile command failed")
 
     click.echo("Done.")
@@ -146,7 +148,7 @@ def install_dependencies():
 def gen_plugin_info():
     """Auto generate the 'plugin.json' file for Flow."""
 
-    with open(basedir / "plugin.json", "w") as f:
+    with open(plugin_info_path, "w") as f:
         json.dump(plugin_infos, f, indent=4)
 
     click.echo("Done.")
